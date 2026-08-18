b5597be60007ccfa74d13004546644c8f7c6da4f
Bisrotex-Invest <bisrotexinvestmentsynergy@gmail.com>
2026-08-18 20:40:46 +0100

    final config file

--- app/config.ts
+++ app/config.ts
@@ -1,5 +1,9 @@
-export const API_URLS = {
-  base: "https://your-api-url.com",
-};
-
-export const OPT_OUTS = {};
+export default {
+  firebaseApiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  firebaseAuthDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  firebaseProjectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  firebaseStorageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  firebaseMessagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  firebaseAppId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  apiUrl: "https://your-api-url.com",
+}
